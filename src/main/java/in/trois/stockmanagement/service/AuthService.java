0c282c3431c5f144efaa5c87e17c48efe73acd5d
MuhammedShafeel <muhammed.shafeel@trois.in>
2025-08-05 17:35:52 +0530

    Changed user to customer

--- src/main/java/in/trois/stockmanagement/service/AuthService.java
+++ src/main/java/in/trois/stockmanagement/service/AuthService.java
@@ -2,48 +2,38 @@ package in.trois.stockmanagement.service;
 
 
 import in.trois.stockmanagement.annotation.ReadTransactional;
-import in.trois.stockmanagement.entity.User;
-import in.trois.stockmanagement.entity.master.Role;
+import in.trois.stockmanagement.entity.Customer;
 import in.trois.stockmanagement.exception.RestException;
-import in.trois.stockmanagement.repository.UserRepository;
+import in.trois.stockmanagement.repository.CustomerRepository;
 import in.trois.stockmanagement.response.AuthResponseDto;
-
 import in.trois.stockmanagement.utils.JwtUtil;
-import in.trois.stockmanagement.utils.ValidationUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final CustomerRepository customerRepository;
 
-    @Autowired
-    private JwtUtil jwtUtil;
+    private final JwtUtil jwtUtil;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
     @ReadTransactional
     public AuthResponseDto login(String username, String password) {
         AuthResponseDto responseDto = new AuthResponseDto();
-        User user =
-                userRepository
+        Customer customer =
+                customerRepository
                         .findByUsername(username)
                         .orElseThrow(() -> new RestException("User not found"));
-        if (!passwordEncoder.matches(password, user.getPassword())) {
+        if (!passwordEncoder.matches(password, customer.getPassword())) {
             throw new RestException("Invalid credentials");
         }
-        if (!ValidationUtils.isValid(user.getRole())) {
-            throw new RestException("Wait for admin to assign you a role");
-        }
-        Role role = user.getRole();
-        responseDto.setUsername(user.getUsername());
-        responseDto.setFullName(user.getFullName());
-        responseDto.setRole(role.toDTO());
-        responseDto.setToken(jwtUtil.generateToken(username, role.getName()));
+        responseDto.setUsername(customer.getUsername());
+        responseDto.setFullName(customer.getFullName());
+        responseDto.setToken(jwtUtil.generateToken(username, "Customer"));
         return responseDto;
     }
 }
